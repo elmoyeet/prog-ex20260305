1c0c5bee18b9b8c5c2ff7a7cfbc4eec742be1d83
elmoyeet <100579650+elmoyeet@users.noreply.github.com>
2026-03-05 09:38:14 +0100

    feat: Objeto CentroFP creado, 3 alumnos registrados, alumnos registrados mostrados, numero total de alumnos mostrado, busqueda existente y busqueda inexistente realizada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main  {
     public static void main(String[] args) {
-        
+
+        CentroFP centroFP = new CentroFP(10);
+
+        Alumno alumno1 = new Alumno(1, "Mario", "DAM", 19);
+        Alumno alumno2 = new Alumno(2, "Papu", "DAM", 18);
+        Alumno alumno3 = new Alumno(3, "Victor", "DAM", 19);
+
+        System.out.println(centroFP.registrarAlumno(alumno1));
+        System.out.println(centroFP.registrarAlumno(alumno2));
+        System.out.println(centroFP.registrarAlumno(alumno3));
+
+        System.out.println(centroFP.mostrarAlumnos());
+
+        System.out.println(centroFP.contarAlumnos());
+
+        System.out.println(centroFP.buscarAlumno(1));
+        System.out.println(centroFP.buscarAlumno(4));
     }
 }
