d0292f48102614e6a3637cc3aca8861e76e7cf2a
elmoyeet <100579650+elmoyeet@users.noreply.github.com>
2026-03-05 08:49:03 +0100

    feat: Constructor completo y metodos getters creados

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,2 +1,32 @@
 public class Alumno {
+
+    private int id;
+    private String nombre;
+    private String tituloFP;
+    int edad;
+
+    public Alumno(int id, String nombre, String tituloFP, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tituloFP = tituloFP;
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTituloFP() {
+        return tituloFP;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+
 }
