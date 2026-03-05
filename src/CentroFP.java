e0504640f06d7c12b2feb7efa94433bb96e1bbc7
elmoyeet <100579650+elmoyeet@users.noreply.github.com>
2026-03-05 09:01:55 +0100

    feat: Método buscarAlumno() creado en clase CentroFP

--- src/CentroFP.java
+++ src/CentroFP.java
@@ -8,5 +8,19 @@ public class CentroFP {
         alumnos = new Alumno[MAX_ALUMNOS];
     }
 
+    public Alumno buscarAlumno(int id) {
+
+        for (int i = 0; i < MAX_ALUMNOS; i++) {
+            if (alumnos[i] != null) {
+                if (alumnos[i].getId() == id) {
+                    return alumnos[i];
+                }
+            }
+        }
+        return null;
+    }
+
+
+
 
 }
