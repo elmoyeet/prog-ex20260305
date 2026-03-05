70251c8eaff41ff830651546ef3375db0e4248f3
elmoyeet <100579650+elmoyeet@users.noreply.github.com>
2026-03-05 10:07:40 +0100

    docs: documentación de los métodos de Alumno agregada

--- src/Alumno.java
+++ src/Alumno.java
@@ -12,22 +12,42 @@ public class Alumno {
         this.edad = edad;
     }
 
+    /**
+     * Getter de ID
+     * @return devuelve la id del alumno
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Getter de nombre
+     * @return devuelve el nombre del alumno
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Getter de titulo
+     * @return devuelve el titulo del alumno
+     */
     public String getTituloFP() {
         return tituloFP;
     }
 
+    /**
+     * Getter de edad
+     * @return devuelve la edad del alumno
+     */
     public int getEdad() {
         return edad;
     }
 
+    /**
+     * Ayuda a generar toda la información del alumno en un mismo string
+     * @return devuelve un string con los datos del alumno
+     */
     @Override
     public String toString() {
         StringBuilder  sb = new StringBuilder("Alumno: {");
