71d3b89d95d72b6a5956154672657e230b5d53e5
elmoyeet <100579650+elmoyeet@users.noreply.github.com>
2026-03-05 08:51:13 +0100

    feat: Atributos creados y array inicializado en el constructor

--- src/CentroFP.java
+++ src/CentroFP.java
@@ -1,2 +1,12 @@
 public class CentroFP {
+
+    private Alumno[] alumnos;
+    private final int MAX_ALUMNOS;
+
+    public CentroFP(int max_alumnos) {
+        MAX_ALUMNOS = max_alumnos;
+        alumnos = new Alumno[MAX_ALUMNOS];
+    }
+
+
 }
